b4da96cc9072f22f8d34ea2f2b41fe145bf8412d
william <batchayonwilliam@gmail.com>
2025-03-03 00:08:49 +0100

    Variable definitions

---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "openstack_username" {
+  description = "OpenStack username"
+  type        = string
+}
+
+variable "openstack_tenant" {
+  description = "OpenStack tenant name"
+  type        = string
+}
+
+variable "openstack_password" {
+  description = "OpenStack password"
+  type        = string
+}
+
+variable "openstack_auth_url" {
+  description = "OpenStack auth URL"
+  type        = string
+}
